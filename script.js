017b779fa4673d2ddbe3be85ea87f4a0d589490c
Jacksondevv <cotchoco@hotmail.com>
2026-03-09 19:48:39 -0300

    Acrescente a primeira parte do site para testes incluindo display

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const botaoHamburguer = document.getElementById("botaoHamburguer");
 const menuDeslizante = document.getElementById("menuDeslizante");
 
 
-/* MENU */
 botaoHamburguer.addEventListener("click", ()=>{
 botaoHamburguer.classList.toggle("ativo");
 menuDeslizante.classList.toggle("ativo");
